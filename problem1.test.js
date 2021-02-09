eb651b48300a9d27071ab312cc60447d9e2bdfc5
Satyam Patel <56593183+satyam-7777@users.noreply.github.com>
2021-02-09 16:17:36 +0530

    Test.js files

---
+++ problem1.test.js
@@ -0,0 +1,4 @@
+
+const problem1 = require('./problem1.js')
+const inventory=require('./inventory.js')
+console.log(problem1);
---
+++ problem2.test.js
@@ -0,0 +1,4 @@
+
+const problem1 = require('./problem2.js')
+const inventory=require('./inventory.js')
+console.log(problem2(inventory));
